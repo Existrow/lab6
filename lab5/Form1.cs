f92051567591b82f1355248eaf2a0e8d9d9812a6
Existrow <49468734+Existrow@users.noreply.github.com>
2021-01-11 15:23:57 +0500

    c
    d

---
+++ lab5/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.Service1Client service = new ServiceReference1.Service1Client("TcpDot");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = service.GetData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            service.SetData(textBox1.Text, textBox2.Text);
+        }
+    }
+}
